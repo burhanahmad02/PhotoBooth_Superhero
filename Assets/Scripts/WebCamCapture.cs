f36f769c4e5d0f9cb2198a91f918b6cf3d9416e8
Muhammad Burhan Ahmad <57236126+burhanahmad02@users.noreply.github.com>
2025-07-07 17:09:28 +0400

    Capture webcam image and encode it to PNG format

--- Assets/Scripts/WebCamCapture.cs
+++ Assets/Scripts/WebCamCapture.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -31,11 +32,41 @@ public class WebcamCapture : MonoBehaviour
         }
         manButton.onClick.AddListener(() => SelectGender("man"));
         womanButton.onClick.AddListener(() => SelectGender("woman"));
+
+        captureButton.onClick.AddListener(() =>
+        {
+            if (!string.IsNullOrEmpty(selectedGender))
+                StartCoroutine(WaitAndCapture(3f));
+            else
+                Debug.LogWarning("Please select a gender before taking a picture.");
+        });
     }
     void SelectGender(string gender)
     {
         selectedGender = gender;
         Debug.Log($"Gender selected: {gender}");
     }
+
+    IEnumerator WaitAndCapture(float delaySeconds)
+    {
+        Debug.Log("Capture initiated. Waiting " + delaySeconds + " seconds...");
+        yield return new WaitForSeconds(delaySeconds);
+        yield return StartCoroutine(CaptureAndSend());
+    }
+
+    IEnumerator CaptureAndSend()
+    {
+        yield return new WaitForEndOfFrame();
+
+        Texture2D texture = new Texture2D(webCamTexture.width, webCamTexture.height);
+        texture.SetPixels(webCamTexture.GetPixels());
+        texture.Apply();
+
+        byte[] bytes = texture.EncodeToPNG();
+
+        // Log to confirm
+        Debug.Log($"Captured image size: {bytes.Length} bytes");
+    }
+
 
 }
